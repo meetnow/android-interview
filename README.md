786749d2da928aa5ef8d00db29a998a281c258b8
sebastianmeetnow <sebastian.otto@meetnow.eu>
2016-02-03 04:40:55 +0700

    feat: add intital android project

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # android-interview
+
+# Steps
+1. Clone this repo
+2. Write code
+3. Test code
+In the project directory you should be able to run the tests:
+```bash
+./gradlew clean test
+```
+4. Submit pull request
+
---
+++ app/src/test/java/eu/meetnow/androidinterview/ExampleUnitTest.java
@@ -0,0 +1,18 @@
+package eu.meetnow.androidinterview;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
